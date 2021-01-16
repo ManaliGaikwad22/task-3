228d50bd9171b3d195c042c8ab957c0ee60be29a
ManaliGaikwad22 <77033185+ManaliGaikwad22@users.noreply.github.com>
2021-01-16 12:48:17 -0500

    Add files via upload

---
+++ nineques.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class nineques
+{
+    public static void main(String[] args) {
+        int i, j, num, n;
+        for (i = 0; i < n; i++) {
+            num = 1;
+            for (j = 0; j <= 1; j++) {
+                System.out.println(num + " ");
+                num++;
+            }
+            System.out.println();
+
+        {
+            int n = 10;
+            System.out.println(n);
+        }
+    }
+}
+
